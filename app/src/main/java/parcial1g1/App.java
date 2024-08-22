12c935d2e5f174ebef708796767cfda7e0c2b3eb
juanp-rojash <juanp.rojash@upb.edu.co>
2024-08-22 00:01:20 -0500

    Estructura Parcial

---
+++ app/src/main/java/parcial1g1/App.java
@@ -0,0 +1,24 @@
+
+package parcial1g1;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        // Implementar la logica de solucion
+        
+    }
+
+    /*  Implemente una funcion llamada calcularConsumoGasolina que cumpla con las siguientes caracteristicas
+        * Recibe: 
+            - Un float representando los km recorridos
+        * Retorna:
+            - Un float representando los litros de gasolina consumidos
+    */
+    //------------------------------------------------------------------
+    //Implemente la funcion en este espacio
+    
+
+    //------------------------------------------------------------------
+
+}
